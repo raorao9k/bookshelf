ff8e47b731bc2ce892b123ede784a89b786359b1
Akshay Rao <66259083+raorao9k@users.noreply.github.com>
2021-10-20 16:16:01 -0400

    Add files via upload

--- bookshelf.py
+++ bookshelf.py
@@ -1,95 +1,161 @@
 '''
     File Name:  bookshelf.py
     Author:     Akshay Rao
-    Date:       September 22, 2021
+    Date:       October 20, 2021
     Modified:   None
-    Copyright Akshay Rao, 2021
+    Copyright Akshay Rao 2021
 '''
 
 import csv
 
 filename = "bookshelf.csv"
-header = ("TITLE", "AUTHOR", "STATUS")
+headers = ["TITLE",
+          "LAST NAME",
+          "FIRST NAME",
+          "SHELF",
+          "SECTION",
+          "FORMAT",
+          "GENRE",
+          "SERIES",
+          "SERIES#",
+          "NOTES"]
+entry = ["", "", "", "", "", "", "", "", "", ""]
+shelves = ["Abandoned",
+           "Gift",
+           "Potential",
+           "Read",
+           "Reading",
+           "Recommended",
+           "Purchased",
+           "To Gift",
+           "To Listen",
+           "To Read"]
+sections = ["9-12",
+            "Kids",
+            "Fiction",
+            "Non-fiction",
+            "Teen",
+            "Well-being"]
+formats = ["Audiobook",
+           "Ebook",
+           "Hardcover",
+           "Paperback"]
+genres = ["Classic",
+          "Crime",
+          "Fantasy",
+          "Historical",
+          "Horror",
+          "Memoir",
+          "Mystery",
+          "Puzzles",
+          "Romance",
+          "Sci-fi",
+          "Self Help"
+          "Thriller",
+          "Western"]
 
 def main():
     '''
-        Print program options for the user and collect the user's option.
-        Repeat until the user inputs the option to exit the program.
+        Run a continuous loop until user exits with keyboard input.
     '''
 
-    'createFile()'
+    userInterface()
 
-    print("\n************************************************************************************")
-    print("* Akshay's Bookshelf ***************************************************************")
-    print("************************************************************************************")
+def printAllSeries():
+    '''
+        Iterate through csv file and print header row and all rows in which "SERIES" is not an empty string.
+    '''
+    count = 1
+    print("******************************************************************************************************************************************************")
+    with open(filename) as file:
+            data = csv.reader(file)
+            for row in data:
+                if row[7] != '':
+                    print('{:<25} {:<45} {:<9} {:<13} {:<35}'.format(row[7],row[0],row[8],row[3],row[9]))
+                    if count == 1:
+                        print("******************************************************************************************************************************************************")
+                        count = count + 1
+
+def userInterface():
+    '''
+        Print program options for the user and collect the user's option. Repeat until the user inputs the option to exit the program.
+    '''
+    print("****************************************************************************************************************************************************************************************")
+    print("*** Akshay's Bookshelf *****************************************************************************************************************************************************************")
+    print("****************************************************************************************************************************************************************************************")
 
     option = '0'
 
-    while (option != '8'):
+    while (option != '9'):
         print("\n")
         print("1 - Add book\n")
         print("2 - Remove book\n")
-        print("3 - Find book\n")
-        print("4 - Find author\n")
-        print("5 - Find status\n")
-        print("6 - Update status\n")
+        print("3 - Find book by title\n")
+        print("4 - Find books by author last name\n")
+        print("5 - Find books by shelf\n")
+        print("6 - Move book to another shelf\n")
         print("7 - List all books\n")
-        print("8 - Exit program\n\n")
+        print("8 - List all series\n")
+        print("9 - Exit program\n\n")
 
         option = input("What do? ")
-        print("\n************************************************************************************")
+        print("****************************************************************************************************************************************************************************************")
         
         if option == '1':
             addBook()
         elif option == '2':
             removeBook()
         elif option == '3':
-            findBy(0)
+            listBy(1)
         elif option == '4':
-            findBy(1)
+            listBy(2)
         elif option == '5':
-            findBy(2)
+            listBy(4)
         elif option == '6':
-            newStatus()
+            changeShelf()
         elif option == '7':
-            listAllBooks()
+            listBy(0)
         elif option == '8':
+            printAllSeries()
+        elif option == '9':
             break
         else:
             print("Invalid option. Select again.\n")
 
-        print("************************************************************************************")
+        print("****************************************************************************************************************************************************************************************")
 
-def createFile():
-    ''' 
-        Create a csv file with name 'filename' if it does not exist in
-        the root directory.
+def createFile(fname):
+    ''' (str) ->
+        Create a csv file with name 'fname'. Write each item in the list 'headers' to the csv file in one row.
     '''
     
-    file = open(filename, 'w', newline='')
-    print(file)
+    file = open(fname, 'w', newline='')
     writer = csv.writer(file)
     writer.writerow(header)
     file.close()
 
 def addBook():
     '''
-        Collect and store new book info from user: title, author, status.
-        Append new book to csv file.
+        Take input strings from user for a new entry append the entry to the csv file.
     '''
-       
-    title = input("Book Title: ")
-    author = input("Book Author: ")
-    status = input("Book Status: ")
+
+    global headers
+    global entry
+
+    count = 0
+    myEntry = ""
+    for x in headers:
+        myEntry = input(x+": ")
+        entry[count] = myEntry
+        count = count + 1
 
     with open(filename, 'a+', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([title,author,status])
+        writer.writerow(entry)
 
 def removeBook():
     '''
-        Collect and store book title from user. Find and remove book from
-        csv file.        
+        Collect and store book title from user. Find and remove book from csv file.        
     '''
 
     title = input("Book Title: ")
@@ -106,36 +172,55 @@ def removeBook():
         data = csv.writer(file)
         data.writerows(myData)
 
-def findBy(index):
+def listBy(index):
     ''' (int) ->
         'index' determines whether to search the csv file by title, author, or status.
-            0 - By title
-            1 - By author
-            2 - By status
+            0 - List all
+                    Read all books from csv file and print to terminal row by row with formatting.
+            1 - By title
+            2 - By author last name
+            3 - By author first name
+            4 - By shelf
     '''
 
     key = "myKey"
-    if index == 0:
-        key = input("Book Title: ")
-    elif index == 1:
-        key = input("Author: ")
-    elif index == 2:
-        key = input("Book Status: ")
-    else:
-        print("ERRORRRRRR!")
-    print("************************************************************************************")
-
     count = 0
-    with open(filename) as file:
-        data = csv.reader(file)
-        for row in data:
-            if count == 0 or row[index] == key:
-                print('{:<45} {:<25} {:<25}'.format(*row))
-            if count == 1:
-                print("************************************************************************************")
-            count = count + 1
+    
+    if index == 0:
+        key = "all" 
+    elif index == 1:
+        key = input("Book Title: ")
+    elif index == 2:
+        key = input("Author Last Name: ")
+    elif index == 3:
+        key = input("Author First Name: ")
+    elif index == 4:
+        key = input("Book Shelf: ")
+    else:
+        key = "error"
+    
+    if index == 0:
+        with open(filename) as file:
+            data = csv.reader(file)
+            for row in data:
+                if count == 1:
+                    print("****************************************************************************************************************************************************************************************")
+                print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
+                count = count + 1
+    else:
+        print("****************************************************************************************************************************************************************************************")
+        with open(filename) as file:
+            data = csv.reader(file)
+            for row in data:
+                if count == 0:
+                    print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
+                    print("****************************************************************************************************************************************************************************************")                 
+                if row[index-1] == key:
+                    print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
+                count = count + 1
 
-def newStatus():
+
+def changeShelf():
     '''
         Collect and store existing book title and its new status from the user.
         Search csv file using the book title and store all book info in 'myRow'.
@@ -144,7 +229,7 @@ def newStatus():
     '''
 
     title = input("Book Title: ")
-    status = input("New Status: ")
+    shelf = input("New Shelf: ")
 
     myRow = []
     myData = []
@@ -155,29 +240,11 @@ def newStatus():
         for row in data:
             if row[0] == title:
                 myRow = row
-                myRow[2] = status
+                myRow[3] = shelf
                 myData.append(myRow)
-                flag = 1
-                break
-            if flag == 0:
+            else:
                 myData.append(row)
 
     with open(filename, 'w') as file:
         data = csv.writer(file)
         data.writerows(myData)
-
-def listAllBooks():
-    '''
-        Read all books from csv file and print to terminal row by row
-        with formatting.
-    '''
-
-    count = 0
-    with open(filename) as file:
-        data = csv.reader(file)
-        for row in data:
-            if count == 1:
-                print("************************************************************************************")
-            print('{:<45} {:<25} {:<25}'.format(*row))
-            count = count + 1
-    print("************************************************************************************")
