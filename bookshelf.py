02540505559fcd42f9ad0b7bb8b1b0c8ed005f7a
Akshay Rao <66259083+raorao9k@users.noreply.github.com>
2021-10-20 20:39:32 -0400

    Create general function searchFile()

--- bookshelf.py
+++ bookshelf.py
@@ -86,17 +86,14 @@ def userInterface():
 
     option = '0'
 
-    while (option != '9'):
+    while (option != '6'):
         print("\n")
         print("1 - Add book\n")
         print("2 - Remove book\n")
-        print("3 - Find book by title\n")
-        print("4 - Find books by author last name\n")
-        print("5 - Find books by shelf\n")
-        print("6 - Move book to another shelf\n")
-        print("7 - List all books\n")
-        print("8 - List all series\n")
-        print("9 - Exit program\n\n")
+        print("3 - Find book\n")
+        print("4 - Move book to another shelf\n")
+        print("5 - List all series\n")
+        print("6 - Exit program\n\n")
 
         option = input("What do? ")
         print("****************************************************************************************************************************************************************************************")
@@ -106,18 +103,12 @@ def userInterface():
         elif option == '2':
             removeBook()
         elif option == '3':
-            listBy(1)
+            searchFile()
         elif option == '4':
-            listBy(2)
-        elif option == '5':
-            listBy(4)
-        elif option == '6':
             changeShelf()
-        elif option == '7':
-            listBy(0)
-        elif option == '8':
+        elif option == '5':
             printAllSeries()
-        elif option == '9':
+        elif option == '6':
             break
         else:
             print("Invalid option. Select again.\n")
@@ -172,53 +163,30 @@ def removeBook():
         data = csv.writer(file)
         data.writerows(myData)
 
-def listBy(index):
-    ''' (int) ->
-        'index' determines whether to search the csv file by title, author, or status.
-            0 - List all
-                    Read all books from csv file and print to terminal row by row with formatting.
-            1 - By title
-            2 - By author last name
-            3 - By author first name
-            4 - By shelf
+def searchFile():
+    '''
+        Take keyword user input from user and search csv file row by row for keyword in all strings.
+        Print row to screen if keyword found.
     '''
 
-    key = "myKey"
     count = 0
-    
-    if index == 0:
-        key = "all" 
-    elif index == 1:
-        key = input("Book Title: ")
-    elif index == 2:
-        key = input("Author Last Name: ")
-    elif index == 3:
-        key = input("Author First Name: ")
-    elif index == 4:
-        key = input("Book Shelf: ")
-    else:
-        key = "error"
-    
-    if index == 0:
-        with open(filename) as file:
-            data = csv.reader(file)
-            for row in data:
-                if count == 1:
-                    print("****************************************************************************************************************************************************************************************")
-                print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
-                count = count + 1
-    else:
-        print("****************************************************************************************************************************************************************************************")
-        with open(filename) as file:
-            data = csv.reader(file)
-            for row in data:
-                if count == 0:
-                    print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
-                    print("****************************************************************************************************************************************************************************************")                 
-                if row[index-1] == key:
-                    print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
-                count = count + 1
+    headerCount = 0
+    myEntry = input("Search term: ")
 
+    print("****************************************************************************************************************************************************************************************")
+
+    with open(filename) as file:
+            data = csv.reader(file)
+            for row in data:
+                if headerCount == 0:
+                    headerCount = headerCount + 1
+                    print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(headers[0],headers[1],headers[2],headers[3],headers[7],headers[9]))
+                    print("****************************************************************************************************************************************************************************************")       
+                for x in row:
+                    if (myEntry in x) and count == 0:
+                        count = count + 1
+                        print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
+                count = 0
 
 def changeShelf():
     '''
