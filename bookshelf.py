8b1586d92423a1575b88a53df0a8e3567ebab238
Akshay Rao <66259083+raorao9k@users.noreply.github.com>
2021-12-24 10:43:20 -0500

    Reformat searchFile(), create printBooks()

--- bookshelf.py
+++ bookshelf.py
@@ -1,7 +1,7 @@
 '''
     File Name:  bookshelf.py
     Author:     Akshay Rao
-    Date:       October 20, 2021
+    Date:       December 24, 2021
     Modified:   None
     Copyright Akshay Rao 2021
 '''
@@ -21,13 +21,11 @@ headers = ["TITLE",
           "NOTES"]
 entry = ["", "", "", "", "", "", "", "", "", ""]
 shelves = ["Abandoned",
-           "Gift",
            "Potential",
            "Read",
            "Reading",
            "Recommended",
            "Purchased",
-           "To Gift",
            "To Listen",
            "To Read"]
 sections = ["9-12",
@@ -55,36 +53,37 @@ genres = ["Classic",
           "Western"]
 
 def main():
-    '''
-        Run a continuous loop until user exits with keyboard input.
+    ''' (NoneType) -> NoneType
+        Call the user interface function.
     '''
 
     userInterface()
 
 def printAllSeries():
-    '''
-        Iterate through csv file and print header row and all rows in which "SERIES" is not an empty string.
+    ''' (NoneType) -> NoneType
+        Iterate through the csv file and print header row and all rows in which "SERIES" is not an empty string.
     '''
     count = 1
-    print("******************************************************************************************************************************************************")
+    print("*****************************************************************************************************************************************************************************************************")
     with open(filename) as file:
             data = csv.reader(file)
             for row in data:
                 if row[7] != '':
-                    print('{:<25} {:<45} {:<9} {:<13} {:<35}'.format(row[7],row[0],row[8],row[3],row[9]))
+                    print('{:<35} {:<45} {:<9} {:<13} {:<35}'.format(row[7],row[0],row[8],row[3],row[9]))
                     if count == 1:
-                        print("******************************************************************************************************************************************************")
+                        print("*****************************************************************************************************************************************************************************************************")
                         count = count + 1
 
 def userInterface():
-    '''
+    ''' (NoneType) -> NoneType
         Print program options for the user and collect the user's option. Repeat until the user inputs the option to exit the program.
     '''
-    print("****************************************************************************************************************************************************************************************")
-    print("*** Akshay's Bookshelf *****************************************************************************************************************************************************************")
-    print("****************************************************************************************************************************************************************************************")
+    print("*****************************************************************************************************************************************************************************************************")
+    print("*** Akshay's Bookshelf ******************************************************************************************************************************************************************************")
+    print("*****************************************************************************************************************************************************************************************************")
 
     option = '0'
+    searchResults = []
 
     while (option != '6'):
         print("\n")
@@ -96,14 +95,15 @@ def userInterface():
         print("6 - Exit program\n\n")
 
         option = input("What do? ")
-        print("****************************************************************************************************************************************************************************************")
+        print("*****************************************************************************************************************************************************************************************************")
         
         if option == '1':
             addBook()
         elif option == '2':
             removeBook()
         elif option == '3':
-            searchFile()
+            searchResults = searchFile()
+            printBooks(searchResults)
         elif option == '4':
             changeShelf()
         elif option == '5':
@@ -113,11 +113,11 @@ def userInterface():
         else:
             print("Invalid option. Select again.\n")
 
-        print("****************************************************************************************************************************************************************************************")
+        print("*****************************************************************************************************************************************************************************************************")
 
 def createFile(fname):
-    ''' (str) ->
-        Create a csv file with name 'fname'. Write each item in the list 'headers' to the csv file in one row.
+    ''' (str) -> NoneType
+        Create a csv file with name fname. Write each item in the global list headers to the csv file in one row.
     '''
     
     file = open(fname, 'w', newline='')
@@ -126,8 +126,8 @@ def createFile(fname):
     file.close()
 
 def addBook():
-    '''
-        Take input strings from user for a new entry append the entry to the csv file.
+    ''' (NoneType) -> NoneType
+        Take input strings from the user for a new entry and append the entry to the csv file.
     '''
 
     global headers
@@ -145,8 +145,8 @@ def addBook():
         writer.writerow(entry)
 
 def removeBook():
-    '''
-        Collect and store book title from user. Find and remove book from csv file.        
+    ''' (NoneType) -> NoneType
+        Collect and store the book title from user. Find and remove the book from the csv file.        
     '''
 
     title = input("Book Title: ")
@@ -164,36 +164,41 @@ def removeBook():
         data.writerows(myData)
 
 def searchFile():
-    '''
-        Take keyword user input from user and search csv file row by row for keyword in all strings.
-        Print row to screen if keyword found.
+    ''' (NoneType) -> list of [str]
+        Take user input for the search term. Search the csv file row by row for the keyword. If found, append the row as an item in myResults.
+        Return myResults.
     '''
 
     count = 0
-    headerCount = 0
+    myResults = []
     myEntry = input("Search term: ")
 
-    print("****************************************************************************************************************************************************************************************")
+    print("*****************************************************************************************************************************************************************************************************")
 
     with open(filename) as file:
             data = csv.reader(file)
             for row in data:
-                if headerCount == 0:
-                    headerCount = headerCount + 1
-                    print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(headers[0],headers[1],headers[2],headers[3],headers[7],headers[9]))
-                    print("****************************************************************************************************************************************************************************************")       
                 for x in row:
                     if (myEntry in x) and count == 0:
+                        myResults.append(row)
                         count = count + 1
-                        print('{:<50} {:<20} {:<20} {:<15} {:<30} {:<35}'.format(row[0],row[1],row[2],row[3],row[7],row[9]))
                 count = 0
+    return myResults
 
-def changeShelf():
+def printBooks(myBooks):
+    ''' (list of list) -> NoneType
+        Print headers and all items in myBooks to screen.
     '''
-        Collect and store existing book title and its new status from the user.
-        Search csv file using the book title and store all book info in 'myRow'.
-        Write new status to 'myRow' and open csv file to replace the row entry
-        of the book.
+
+    print('{:<50} {:<35} {:<15} {:<12} {:<30} {:<35}'.format(headers[0],headers[1],headers[2],headers[3],headers[7],headers[9]))    
+    print("*****************************************************************************************************************************************************************************************************")
+    for book in myBooks:
+        print('{:<50} {:<35} {:<15} {:<12} {:<30} {:<35}'.format(book[0],book[1],book[2],book[3],book[7],book[9]))
+    
+def changeShelf():
+    ''' (NoneType) -> NoneType
+        Collect and store existing book title and its new status from the user. Search csv file using the book title and store all book info in 'myRow'.
+        Write new status to myRow and open csv file to replace the row entry of the book.
     '''
 
     title = input("Book Title: ")
